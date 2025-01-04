ef4b2052e0863bb00651d6df93b269f659beee59
shreyas <sbtarar430@gmail.com>
2025-01-04 17:11:51 +0530

    project setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import RootStack from './navigtion/RootStack';
+
+const App = () => {
+  return (
+    <RootStack />
+  );
+};
+
+export default App;
---
+++ src/navigtion/ScreenName.js
@@ -0,0 +1,9 @@
+export const AUTHSTACK = 'authstack';
+export const BOTTOMTAB = 'bottomTab';
+export const SPLASH = 'splash';
+export const SIGNIN = 'signin';
+export const SIGNUP = 'signup';
+export const HOME = 'home';
+export const COURSE = 'course';
+export const CATALOG = 'catalog';
+export const ACCOUNT = 'account';
